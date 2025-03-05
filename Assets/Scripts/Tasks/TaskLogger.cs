fe9302bc4aed94cf1fb22ef80eaeb25a35423573
efrosim <stolyarova.evgeny@yandex.ru>
2025-03-05 13:43:38 +0300

    Update Logger: add config for custom log path

--- Assets/Scripts/Tasks/TaskLogger.cs
+++ Assets/Scripts/Tasks/TaskLogger.cs
@@ -1,15 +1,70 @@
 using System;
 using System.IO;
 using UnityEngine;
+using static Valve.VR.SteamVR_ExternalCamera;
 
 public static class TaskLogger
 {
     private static string logFilePath;
 
+    [Serializable]
+    private class TaskLoggerConfig
+    {
+        [NonSerialized]
+        public string logConfigFolderPath;
+        [NonSerialized]
+        public string logConfigFilePath;
+        public string editorLogFolderPath;
+        public string buildLogFolderPath;
+
+        public TaskLoggerConfig()
+        {
+            logConfigFolderPath = Path.Combine(Application.persistentDataPath, "TaskLoggerConfig");
+            logConfigFilePath = Path.Combine(logConfigFolderPath, "config.json");
+        }
+
+        public void CreateConfigFile()
+        {
+            editorLogFolderPath = Path.Combine(Application.persistentDataPath, "EditorTaskLogs");
+            buildLogFolderPath = Path.Combine(Application.persistentDataPath, "BuildTaskLogs");
+            string json = JsonUtility.ToJson(this);
+            if (!Directory.Exists(logConfigFolderPath)) Directory.CreateDirectory(logConfigFolderPath);
+            File.WriteAllText(logConfigFilePath, json);
+        }
+    }
+
     static TaskLogger()
     {
+        TaskLoggerConfig config = new TaskLoggerConfig();
+
         string timestamp = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
-        logFilePath = Path.Combine(Application.persistentDataPath, $"TaskLog_{timestamp}.txt");
+        Debug.Log("Log config file path: " + config.logConfigFilePath);
+
+        if (!File.Exists(config.logConfigFilePath))
+        {
+            config.CreateConfigFile();
+        }
+        else
+        {
+            string json = File.ReadAllText(config.logConfigFilePath);
+            config = JsonUtility.FromJson<TaskLoggerConfig>(json);
+            Debug.Log("Loaded config: " + config.editorLogFolderPath);
+        }
+
+        string logFolder;
+
+#if UNITY_EDITOR
+        logFolder = config.editorLogFolderPath;
+#else
+        logFolder = config.buildLogFolderPath;
+#endif
+
+        logFilePath = Path.Combine(logFolder, $"TaskLog_{timestamp}.txt");
+
+        if (!Directory.Exists(logFolder))
+        {
+            Directory.CreateDirectory(logFolder);
+        }
         File.WriteAllText(logFilePath, "Task Log - Started at " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "\n\n");
         Debug.Log("TaskLogger initialized: " + logFilePath);
     }
