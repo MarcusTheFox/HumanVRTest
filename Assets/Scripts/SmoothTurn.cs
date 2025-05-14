cc94670a338167fde167c01d3643b7c65781a67a
MarcusTheFox <menjmenj@gmail.com>
2025-05-14 15:09:21 +0300

    Fix SmoothTurn

--- Assets/Scripts/SmoothTurn.cs
+++ Assets/Scripts/SmoothTurn.cs
@@ -1,10 +1,18 @@
+using System;
 using UnityEngine;
 using Valve.VR;
+using Valve.VR.InteractionSystem;
 
 public class SmoothTurn : MonoBehaviour
 {
     [SerializeField] private SteamVR_Action_Vector2 turnAction;
     [SerializeField] private float turnSpeed = 45.0f;
+    private Player player;
+
+    private void Start()
+    {
+        player = Player.instance;
+    }
 
     void Update()
     {
@@ -12,7 +20,12 @@ public class SmoothTurn : MonoBehaviour
 
         if (Mathf.Abs(turnAxis.x) > 0.1f)
         {
-            transform.parent.Rotate(Vector3.up, turnAxis.x * turnSpeed * Time.deltaTime);
+            float angle = turnAxis.x * turnSpeed * Time.deltaTime;
+            Vector3 playerFeetOffset = player.trackingOriginTransform.position - player.feetPositionGuess;
+            player.trackingOriginTransform.position -= playerFeetOffset;
+            player.transform.Rotate(Vector3.up, angle);
+            playerFeetOffset = Quaternion.Euler(0.0f, angle, 0.0f) * playerFeetOffset;
+            player.trackingOriginTransform.position += playerFeetOffset;
         }
     }
 }
